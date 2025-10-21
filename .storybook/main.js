8f2c800fb26b6480eda09737265eec571f920a83
John McCollum <john.mccollum@arnoldclark.com>
2025-10-21 12:11:43 +0100

    Add basepath

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,14 @@
-
-
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": ["@storybook/addon-docs", "@storybook/addon-a11y"],
-  "framework": {
-    "name": "@storybook/html-vite",
-    "options": {}
-  }
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
+  framework: {
+    name: "@storybook/html-vite",
+    options: {},
+  },
+  base: "/chassis-sb/",
 };
 export default config;
